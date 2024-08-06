c61ded29d6e07b902769ace515aff5c2ea15e03a
Michel Manarin <michelmanarin@gmail.com>
2024-08-06 19:00:13 -0300

    fix: repository

--- CloudBooks.API/Presentation/ViewModel/LivrosPorAutorViewModel.cs
+++ CloudBooks.API/Presentation/ViewModel/LivrosPorAutorViewModel.cs
@@ -2,8 +2,5 @@
 {
     public string Autor { get; set; }
     public string Livros { get; set; }
-    public string Editora { get; set; }
-    public int Edicao { get; set; }
-    public string AnoPublicacao { get; set; }
     public string Assuntos { get; set; }
 }
